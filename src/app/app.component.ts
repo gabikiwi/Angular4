45e56e2a63635e621e290cddfad59fcfcc2a97d1
Gabriel Chivoiu <gabriel.chivoiu@oracle.com>
2017-08-14 17:45:37 +0300

    Dockerfile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'Gabriels App';
+obj = {
+	id:"1",
+	name:"nume membrii familie"
+}
+
+arrayVariable = ["ana", "magda", "gabriel"];
+
+isTrue = false;
+myName = 'Chivoiu';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,39 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { ProductComponent } from './product/product.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-	FormsModule,
-	HttpModule　
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		MyComponentComponent,
+		ProductComponent,
+		MembersComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule,
+		RouterModule.forRoot([
+			{
+				path:'member',
+				component:MembersComponent
+			},
+
+			{
+				path:'product',
+				component:ProductComponent
+			}
+
+		])
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
